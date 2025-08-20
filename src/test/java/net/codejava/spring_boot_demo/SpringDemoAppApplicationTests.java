351a5e6855757aaf7049267b46c21c516bff93b2
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2025-08-20 10:33:01 +0800

    Initial Commit

---
+++ src/test/java/net/codejava/spring_boot_demo/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
