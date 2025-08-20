176f45e0aef3f822dc6cbe2031f7fa466c638cf6
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2025-08-20 12:07:46 +0800

    Implemented first API

--- src/main/java/net/codejava/spring_boot_demo/SpringDemoAppApplication.java
+++ src/main/java/net/codejava/spring_boot_demo/SpringDemoAppApplication.java
@@ -2,12 +2,20 @@ package net.codejava.spring_boot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDemoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoAppApplication.class, args);
+        SpringApplication.run(SpringDemoAppApplication.class, args);
 	}
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World Spring Boot";
+    }
 
 }
