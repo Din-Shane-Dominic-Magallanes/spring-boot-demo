537b7e9522fcafeed38d2456b8597093f2fc6f93
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2025-08-20 12:34:43 +0800

    Implemented Software Engineer Object

---
+++ src/main/java/net/codejava/spring_boot_demo/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+package net.codejava.spring_boot_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineer() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "Allana",
+                        "js, node, react, tailwindcss"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Din",
+                        "java, spring, springboot"
+                )
+        );
+    }
+
+}
